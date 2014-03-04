c42857050754025bdcff093b2bfbd0f4b36ef9dc
Dean Chester <dean@maquib.com>
2014-03-04 23:44:44 +0000

    Initial commit of project.

---
+++ main.c
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.c
+ * Author: dean
+ *
+ * Created on March 4, 2014, 10:32 PM
+ */
+
+#include <xc.h>
+#pragma config FOSC = HS        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config CP = OFF         // FLASH Program Memory Code Protection bit (Code protection off)
+#pragma config BOREN = OFF
+
+
+/*
+ * 
+ */
+int main() {
+    TRISA = 0;
+    PORTA = 0x01;
+    while(1){
+        PORTA ^= 0x01;
+        _delay(20000000);
+    }
+}
